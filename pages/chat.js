b0b864d184b1bef44815fe431392594a9d9bdb4a
Kaue Lima <kaue.lima13@gmail.com>
2022-01-25 14:23:17 -0300

    Added router and chat page

---
+++ pages/chat.js
@@ -0,0 +1,5 @@
+export default function chatPage() {
+    return (
+        <div>chatline</div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,7 @@
 import appConfig from '../config.json';
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import React from 'react';
-
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-    );
-  }
+import { useRouter } from 'next/router';
 
 function Title(props){
     const Tag = props.tag || 'h1';
@@ -50,9 +22,10 @@ function Title(props){
 
 export default function PaginaInicial() {
     const [username, setUsername] = React.useState('kauelima');
+    const roteamento = useRouter();
+
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'left',
@@ -72,8 +45,8 @@ export default function PaginaInicial() {
               width: '100%', 
               maxWidth: {
                 xs: '100%', 
-                md: '70%', 
-                lg: '60%', 
+                md: '90%', 
+                lg: '80%', 
               },
               height: "100%",
               padding: '32px', margin: 0,
@@ -84,6 +57,10 @@ export default function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function (eventInfo){
+                eventInfo.preventDefault();
+                roteamento.push("/chat")
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
